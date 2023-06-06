e1c966044a3ca8cfde947de6761d350640a80995
Kaminska-Kit <135758575+Kaminska-Kit@users.noreply.github.com>
2023-06-06 23:05:29 +0300

    Update hw_1.js

--- hw_1.js
+++ hw_1.js
@@ -78,8 +78,7 @@ console.log(`item_6 == ${item_6}, item_6_type == ${item_6_type}`);
 
 // 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 
-let item_7 = item_6.toString();
-// or let item_7 = String(item_6);
+let item_7 = String(item_6);
 
 // 23. Создать переменную item_7_type
 // 24. Присвоить переменной item_7_type тип переменной item_7
