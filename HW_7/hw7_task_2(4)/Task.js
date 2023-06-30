8917a44a4b87c4cd09bb82b4626f56aac64726cd
Kaminska-Kit <135758575+Kaminska-Kit@users.noreply.github.com>
2023-07-01 00:01:20 +0300

    Create Task.js

---
+++ HW_7/hw7_task_2(4)/Task.js
@@ -0,0 +1,8 @@
+export class Task {
+    constructor(featureName, userStoryNumber, estimations) {
+        this.featureName = featureName;
+        this.userStoryNumber = userStoryNumber;
+        this.estimations = estimations;
+    };
+};
+
