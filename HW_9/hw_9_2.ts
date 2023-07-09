cf2cdcabec5a5a1e2ce7c1c22a1ee698912790dc
Kaminska-Kit <135758575+Kaminska-Kit@users.noreply.github.com>
2023-07-10 00:00:09 +0300

    Update hw_9_2.ts

--- HW_9/hw_9_2.ts
+++ HW_9/hw_9_2.ts
@@ -1,10 +1,12 @@
+export {}
+
 // 1. Создать функцию, принимающую на вход массив объектов, у которых ключ ТОЛЬКО string, а значения могут быть string, number, boolean.
 //     Вернуть результирующий объект, хранящий в себе все переданные в функцию объекты справа на лево
 
 type CustomObject = {
     [key: string]: string | number | boolean
 };
-type CustomArray = [...CustomObject[]];
+type CustomArray = CustomObject[];
  
 const obj1: CustomObject = {
     first: 'string'
@@ -33,55 +35,107 @@ console.log(typifiedArray(myArray));
 //   Подумайте, можно ли в данном алиасе выделить части, которые можно вынести в отдельные алиасы. 
 //   Подумайте также, все ли поля должны быть обязательными. 
 //   Ниже представлен пример запроса:
-//   const order = {
-//     customer: {
-//       name: "Anatoly",
-//       surname: "Karpocich",
-//       address: {
-//         city: "Minsk",
-//         street: "Narodnaja",
-//         house: 10,
-//         flat: 99,
-//       },
-//     },
-//     ordered: {
-//       pizzas: [
-//         {
-//           name: "peperoni",
-//           size: "large",
-//           extras: ["cheese", "bacon"],
-//           cost: 100,
-//         },
-//         {
-//           name: "diablo",
-//           size: "medium",
-//           extras: ["halapenio"],
-//           cost: 70,
-//         },
-//         {
-//           name: "hawai",
-//           size: "small",
-//           extras: [],
-//           cost: 50,
-//         },
-//       ],
-//       sauses: ["BBQ", "cheese", "1000 islands"],
-//     },
-//     delivery: {
-//       condition: "DELIVERY",
-//       address: {
-//         city: "Minsk",
-//         street: "Narodnaja",
-//         house: 10,
-//         flat: 99,
-//       },
-//       date: "2023/07/22",
-//       time: "19:00",
-//     },
-//     paymentCondition: "card",
-//     isPaid: true,
-//     totalPrice: 250,
-//   };
+
+interface IOrderResponse extends IOrderRequest {
+    createdAt: string,
+    id: number
+}
+interface IOrderRequest {
+    customer: ICustomer,
+    ordered: IOrdered,
+    delivery: IDelivery,
+    paymentCondition: Payment,
+    isPaid: Boolean,
+    totalPrice: number,
+};
+interface ICustomer {
+    name: string,
+    surname: string,
+    address: IAddress
+};
+interface IOrdered {
+    pizzas: IPizza[],
+    sauces?: Sauces[]
+};
+interface IPizza {
+    name: PizzaNames,
+    size: PizzaSize,
+    extras?: Extras[],
+    cost: number,
+};
+interface IDelivery {
+    condition: CONDITIONS.DELIVERY,
+    address: IAddress
+    date: string,
+    time: string,
+};
+interface IAddress {
+    city: string,
+    street: string,
+    house: number,
+    flat?: number,           
+};
+enum CONDITIONS {
+    DELIVERY = "DELIVERY",
+    DELIVERED = "DELIVERED",
+    IN_PROGRESS = "IN PROGRESS",
+    CANCELED = "CANCELED"
+};
+type Payment = 'card' | 'cash';
+type PizzaSize = 'small' | 'large' | 'medium';
+type Extras = 'cheese' | 'halapenio' | 'bacon' | 'salami' | 'olives' | 'pickles' | 'mushrooms';
+type Sauces = 'BBQ' | 'cheese' | '1000 islands';
+type PizzaNames = 'BBQ' | 'margarita' | 'peperoni' | 'diablo' | 'hawai' | 'vegan';
+
+const order: IOrderRequest = {
+    customer: {
+      name: "Anatoly",
+      surname: "Karpocich",
+      address: {
+        city: "Minsk",
+        street: "Narodnaja",
+        house: 10,
+        flat: 99,
+      },
+    },
+    ordered: {
+      pizzas: [
+        {
+          name: "peperoni",
+          size: "large",
+          extras: ["cheese", "bacon"],
+          cost: 100,
+        },
+        {
+          name: "diablo",
+          size: "medium",
+          extras: ["halapenio"],
+          cost: 70,
+        },
+        {
+          name: "hawai",
+          size: "small",
+          extras: [],
+          cost: 50,
+        },
+      ],
+      sauces: ["BBQ", "cheese", "1000 islands"],
+    },
+    delivery: {
+      condition: CONDITIONS.DELIVERY,
+      address: {
+        city: "Minsk",
+        street: "Narodnaja",
+        house: 10,
+        flat: 99,
+      },
+      date: "2023/07/22",
+      time: "19:00",
+    },
+    paymentCondition: "card",
+    isPaid: true,
+    totalPrice: 250,
+  };
 
 // 3. Напишите функцию, которая принимает строку и возвращает объект со счетчиком того, сколько раз каждая буква встречается в строке. 
 //   Затипизируйте так, чтобы функция принимала только строковые входные данные и чтобы выходной объект имел строковые ключи 
@@ -92,8 +146,8 @@ type CustomObject = {
     };
 
 function countLetters(str: string) {
-    const resultObject: CustomObject = {};
-    const arrayOfLetters: string[] = str.toLowerCase().split('');
+    const resultObject: CustomObject = {}
+    const arrayOfLetters: string[] = str.toLowerCase().split('') 
     const result: number[] = arrayOfLetters.map((char, index, arr) => {
         return arr.filter((el) => el == char).length;
     });
@@ -105,6 +159,7 @@ function countLetters(str: string) {
 console.log(countLetters('pepper'))
 console.log(countLetters('ginger'))
 
+
 // 4.  Реализуйте функцию filter(), которая принимает на вход массив чисел и предикат, 
 //     который будет использоваться для проверки каждого числа на соответствие требованиям. 
 //     Помимо самой функции следует реализовать алиасы типов для функций и аттрибутов. 
@@ -113,3 +168,12 @@ console.log(countLetters('ginger'))
 //     filter(numbers, (n) => n > 3); // [4, 133]
 //     filter(numbers, (n) => n % 2 == 0); // [2, 4]
 //     Параметры функции: Массив чисел и Анонимная функция, принимающая на вход число и возвращающая логическое значение.
+
+type ArrayOfNumbers =  number[];
+type CallBack = (n: number) => boolean;
+
+function filter(numbers: ArrayOfNumbers, callback: CallBack) {
+    return numbers.filter(num => callback(num));
+};
+console.log(filter([1, -5, 2, 3, 4, 133], (n) => n % 2 == 0));
+console.log(filter([1, -5, 2, 3, 4, 133], (n) => n > 3));
